b59aab6314e9ffb5b7a7a03962cc0b9ddcd4893c
nressa <mressanova@gmail.com>
2023-09-04 01:43:46 +0800

    feat: use the EJS for the template On branch feat-dynamic-files modified:   node_modules/.package-lock.json modified:   package-lock.json modified:   package.json modified:   public/about.html modified:   public/style.css modified:   server.js renamed:    public/index.html -> views/index.ejs

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static("public"))
+app.set("view engine", "ejs") // Command express to use EJS for rendering view template
+
+app.get('/', function(req, res) {
+    res.render("index.ejs", {
+        numberOfIterations: 10
+    }); // This will automatically look at the views folder
+});
 
 app.get('/about', function(req, res) {
-  res.sendFile(__dirname + "/public/about.html");
+    res.sendFile(__dirname + "/public/about.html");
 });
+
+app.use(express.static("public"))
 
 const port = 8080
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
