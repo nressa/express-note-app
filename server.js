ac60ea754eabca10a4269fa25df79796ac9568da
nressa <mressanova@gmail.com>
2023-09-04 00:39:30 +0800

    feat: use express static in public folder new file:   public/index.html new file:   public/style.css modified:   server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const app = express()
+
+app.use(express.static("public"))
+
 const port = 8080
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get("/goodbye", (req, res) => {
-    res.send("<h1>Hello new World</h1>")
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
